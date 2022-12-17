c978c746ec5d03c9abcced37a2e6effdff6f4dc8
Brian S J Calderon <93496814+BrianZek@users.noreply.github.com>
2022-12-16 19:37:13 -0600

    Add files via upload

---
+++ JavaBasico/Tema_1/src/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+    public static void main(String[] args) {
+        //Enteros
+        byte var1 = 1;
+        short var2 = 32767;
+        int var3 = 2147483647;
+        long var4 = 922337200;
+        float var5 = 9.28f;
+        double var6 = 16.5436d;
+
+        //logicos
+        boolean verdadero = true;
+        boolean falso = false;
+
+        //Texto
+
+        char var7 = 'a';
+        String var8 = "Brian";
+
+        //Impresion
+
+        System.out.println("Soy una variable typo byte con el valor: " + var1);
+        System.out.println("Soy una variable typo short con el valor: " + var2);
+        System.out.println("Soy una variable typo int con el valor: " + var3);
+        System.out.println("Soy una variable typo long con el valor: " + var4);
+        System.out.println("Soy una variable typo float con el valor: " + var5);
+        System.out.println("Soy una variable typo double con el valor: " + var6);
+        System.out.println("Soy una variable typo boolean con el valor: " + verdadero);
+        System.out.println("Soy una variable typo boolean con el valor: " + falso);
+        System.out.println("Soy una variable typo char con el valor: " + var7);
+        System.out.println("Soy una variable typo String con el valor: " + var8);
+
+
+
+    }
+}
