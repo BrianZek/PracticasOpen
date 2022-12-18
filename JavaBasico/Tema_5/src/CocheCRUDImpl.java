13b846b00acdb5f17567004faad221b28fbb7b41
Brian S J Calderon <93496814+BrianZek@users.noreply.github.com>
2022-12-17 19:12:37 -0600

    Add files via upload

---
+++ JavaBasico/Tema_5/src/CocheCRUDImpl.java
@@ -0,0 +1,17 @@
+public class CocheCRUDImpl implements CocheCRUD {
+
+    @Override
+    public void save() {
+        System.out.println("Este es el metodo save");
+    }
+
+    @Override
+    public void findAll() {
+        System.out.println("Este es el metodo findAll");
+    }
+
+    @Override
+    public void delete() {
+        System.out.println("Este es el metodo delete");
+    }
+}
